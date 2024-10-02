a14a6917f445fa79be846685859307e9fb17f0ee
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-02 15:14:58 +0200

    added comment

--- index.php
+++ index.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
 
-    <!-- BOOTSTRAP 5.3.3 -->
+    <!-- CDN BOOTSTRAP 5.3.3 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
+
     <main>
+
+        <!-- Collego il file censored.php al form tramite action -->
         <form action="./censored.php">
             <div class="mb-3 container text-center">
                 <label for="phrase" class="form-label mt-5 mb-4 h2">FORM</label>
